7d96ff9ac33d6e82226ddf8be8849bb46b45654b
Anthony Ambos <testedelivery@gmail.com>
2026-06-26 20:29:11 -0400

    fix(ios): prevent UI overlapping with notch

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.eprajalojista',
   appName: 'É Pra Já - Lojista',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    StatusBar: {
+      style: 'DARK',
+      overlaysWebView: false,
+    }
+  }
 };
 
 export default config;
